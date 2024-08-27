32530c199abf51f17beaa784bbe2f43029f2da17
jpgomesf <jppromus@gmail.com>
2024-08-27 20:44:20 -0300

    :tada: Begins Airbnb portifolio

---
+++ shuren.io/Core/Listings/ListingItemView.swift
@@ -0,0 +1,72 @@
+//
+//  ListingView.swift
+//  shuren.io
+//
+//  Created by Joao Pedro Gomes Ferreira on 27/08/24.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4",
+    ]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            // images
+            TabView {
+                ForEach(0 ... 3, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            // listing details
+            
+            HStack(alignment: .top) {
+                //details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                 
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                
+                Spacer()
+                
+                // rating
+                
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
