238a9876bfefcc87794f97fd50eb935cc666ad89
jpgomesf <jppromus@gmail.com>
2024-08-27 16:37:06 -0300

    Initial Commit

---
+++ shuren.io/shuren_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  shuren_ioApp.swift
+//  shuren.io
+//
+//  Created by Joao Pedro Gomes Ferreira on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct shuren_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
