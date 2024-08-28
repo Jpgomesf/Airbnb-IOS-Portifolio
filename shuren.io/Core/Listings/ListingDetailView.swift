311a9895e0166cf34d58ef4db2517f7202600181
jpgomesf <jppromus@gmail.com>
2024-08-27 23:45:51 -0300

    :sparkles: add new male stock image assets.

---
+++ shuren.io/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailView.swift
+//  shuren.io
+//
+//  Created by Joao Pedro Gomes Ferreira on 27/08/24.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
